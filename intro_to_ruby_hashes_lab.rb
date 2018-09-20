99c147ad9eb242c9537b0b4b2f0e8d1633d10440
Manny Pissanos <mpissano@gmail.com>
2018-09-20 14:51:08 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -60,9 +60,15 @@ def monopoly_with_fourth_tier
 	      :reading_railroad => {
 	        "mortgage_value"=>"$100"
 	      },
-	      :pennsylvania_railroad => {},
-	      :b_and_o_railroad => {},
-	      :shortline => {},
+	      :pennsylvania_railroad => {
+	        
+	      },
+	      :b_and_o_railroad => {
+	        
+	      },
+	      :shortline => {
+	        
+	      },
 	      
 	    },
 	    :rent_in_dollars => 
